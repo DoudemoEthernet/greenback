c31a48fdfbdff04fce5981d75616221be87cc229
turtton <top.gear7509@turtton.net>
2023-04-24 22:15:49 +0900

    feat: create Credential trait

--- src/db/entity/account.rs
+++ src/db/entity/account.rs
@@ -30,3 +30,66 @@ impl Default for AccountId {
         Self(gen)
     }
 }
+
+#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
+pub struct Username(String);
+
+impl From<Username> for String {
+    fn from(value: Username) -> Self {
+        value.0
+    }
+}
+
+impl AsRef<str> for Username {
+    fn as_ref(&self) -> &str {
+        &self.0
+    }
+}
+
+impl Username {
+    pub fn new(username: String) -> Self {
+        Self(username)
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
+pub struct Password(String);
+
+impl From<Password> for String {
+    fn from(value: Password) -> Self {
+        value.0
+    }
+}
+
+impl AsRef<str> for Password {
+    fn as_ref(&self) -> &str {
+        &self.0
+    }
+}
+
+impl Password {
+    pub fn new(password: String) -> Self {
+        Self(password)
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
+pub struct Credential {
+    account_id: AccountId,
+    username: Username,
+    password: Password
+}
+
+impl Credential {
+    pub fn new(
+        id: impl Into<i64>,
+        username: impl Into<String>,
+        password: impl Into<String>
+    ) -> Self {
+        Self { 
+            account_id: AccountId::new(id.into()), 
+            username: Username::new(username.into()), 
+            password: Password::new(password.into())
+        }
+    }
+}
