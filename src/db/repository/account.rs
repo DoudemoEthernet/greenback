881b9a80c809e16eec9e7cdbe20d0e51c215b7c1
turtton <top.gear7509@turtton.net>
2023-04-25 22:45:12 +0900

    feat: add CredentialRepository

---
+++ src/db/repository/account.rs
@@ -0,0 +1,13 @@
+use async_trait::async_trait;
+
+use crate::db::{
+    entity::account::{AccountId, Credential, Username},
+    error::DatabaseError,
+};
+
+#[async_trait(?Send)]
+pub trait CredentialRepository {
+    async fn create(&self, create: &Credential) -> Result<(), DatabaseError>;
+    async fn get_from_name(&self, name: &Username) -> Result<Credential, DatabaseError>;
+    async fn delete(&self, id: &AccountId) -> Result<(), DatabaseError>;
+}
