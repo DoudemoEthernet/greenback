d5c8d0c10d40ec39ce40aaea49879675e4827447
turtton <top.gear7509@turtton.net>
2023-04-28 09:03:42 +0900

    feat: add jwt generator

--- src/util.rs
+++ src/util.rs
@@ -1,3 +1,4 @@
 pub mod hooks;
 pub mod string;
 pub mod task;
+pub mod jwt;
---
+++ src/util/jwt.rs
@@ -0,0 +1,23 @@
+use jsonwebtoken::{decode, encode, Algorithm, DecodingKey, EncodingKey, Header, Validation};
+use serde::{Deserialize, Serialize};
+use time::OffsetDateTime;
+
+use crate::db::entity::account::Username;
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Claims {
+    sub: String,
+    exp: usize,
+}
+
+pub fn create_token(
+    key: &EncodingKey,
+    username: &Username,
+) -> Result<String, jsonwebtoken::errors::Error> {
+    let claims = Claims {
+        sub: username.to_owned().into(),
+        exp: OffsetDateTime::now_utc().unix_timestamp() as usize,
+    };
+
+    encode(&Header::default(), &claims, &key)
+}
