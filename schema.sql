593b4d744c2f572c28d99b7e29117898391b1d2e
turtton <top.gear7509@turtton.net>
2023-04-29 12:59:45 +0900

    chore: add exp table

--- schema.sql
+++ schema.sql
@@ -18,6 +18,13 @@ CREATE TABLE tasks (
 );
 CREATE INDEX idx_tasks_created_at ON tasks (created_at);
 
+CREATE TABLE exp (
+  username integer PRIMARY KEY NOT NULL,
+  exp integer NOT NULL.
+
+  FOREIGN KEY(username) REFERENCES credentials(username)
+)
+
 INSERT INTO credentials (
   account_id, user_name, password
 ) VALUES ( 0, 'dummy-user', '*********************' )
